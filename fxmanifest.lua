a4b92e1201859b8025653ba272c6fb6df4dfe02f
fxDopa <131190297+rdopaa@users.noreply.github.com>
2023-06-04 04:45:32 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,23 +11,11 @@ version '2.0'
 server_script {
 	'@oxmysql/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/tr.lua',
-	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/tr.lua',
-	'config.lua',
 	'client/main.lua'
 }
 
